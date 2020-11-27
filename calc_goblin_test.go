796574c7c0940fbafad96ae952ad4f0354b86716
jason683 <68846313+jason683@users.noreply.github.com>
2020-11-27 16:55:14 +0800

    Update calc_goblin_test.go

--- calc_goblin_test.go
+++ calc_goblin_test.go
@@ -10,20 +10,20 @@ func TestCalculator(t *testing.T) {
 	g := Goblin(t)
 	g.Describe("Calculator", func() {
 		g.It("should add two numbers ", func() {
-			g.Assert(Add(1, 2)).Equal(3)
-			g.Assert(Add(1, 1)).Equal(2)
+			g.Assert(Add(100, 101)).Equal(201)
+			g.Assert(Add(5, -1)).Equal(4)
 		})
 
 		g.It("should subtract two numbers", func() {
-			g.Assert(Subtract(5, 3)).Equal(2)
+			g.Assert(Subtract(1000, 997)).Equal(3)
 		})
 
 		g.It("should multiply two numbers", func() {
-			g.Assert(Multiply(5, 6)).Equal(30)
+			g.Assert(Multiply(859, 858)).Equal(737022)
 		})
 
 		g.It("should divide two numbers", func() {
-			g.Assert(Divide(10, 2)).Equal(float64(5))
+			g.Assert(Divide(29, 2)).Equal(float64(14.5))
 		})
 	})
 }
