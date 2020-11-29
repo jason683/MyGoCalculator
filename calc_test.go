91a6f4b9bc29099edc358da5bd4867b524fb1c43
jason683 <68846313+jason683@users.noreply.github.com>
2020-11-30 00:06:06 +0800

    Update and rename calc_goblin_test.go to calc_test.go

---
+++ calc_test.go
@@ -0,0 +1,32 @@
+package calc
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMultiply(t *testing.T) {
+	a, b := 6, 5
+	mresult := Multiply(a, b)
+	if mresult <= 0 {
+		t.Error("Value is negative")
+	}
+	if mresult != 30 {
+		t.Errorf("Value of %d is incorrect. Want 30!", mresult)
+	} else {
+		fmt.Println("Result is correct")
+	}
+}
+
+func TestDivide(t *testing.T) {
+	a, b := 6, 5
+	dresult := Divide(a, b)
+	if dresult <= 0 {
+		t.Error("Value is negative")
+	}
+	if dresult != 1.2 {
+		t.Errorf("Value of %.1f is incorrect. Want 1.2!", dresult)
+	} else {
+		fmt.Println("Result is correct")
+	}
+}
