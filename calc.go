6a5b30688fa143a3b343f6ecba79a94405b610ac
jason683 <68846313+jason683@users.noreply.github.com>
2020-11-30 00:05:25 +0800

    Update calc.go

--- calc.go
+++ calc.go
@@ -1,21 +1,10 @@
-package main
+package calc
 
-// Add two numbers and return the results
-func Add(a, b int) int {
-	return a + b
-}
-
-// Subtract two numbers and return the result.
-func Subtract(a, b int) int {
-	return a - b
-}
-
-// Multiply two numbers and return the result.
 func Multiply(a, b int) int {
 	return a * b
 }
 
-// Divide two numbers and return the result.
 func Divide(a, b int) float64 {
-	return float64(a / b)
+	c := float64(a) / float64(b)
+	return float64(c)
 }
